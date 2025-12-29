4d570e936cc511a1c51af32aa765cffbb9888156
Dinar Davis <dinardavis@gmail.com>
2025-12-29 15:14:14 -0800

    Add mailchimp script and newsData clean up

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useEffect } from 'react';
 import IntroHome from './IntroHome';
 import QuoteHome from './QuoteHome';
 import ApproachHome from "./ApproachHome";
@@ -8,6 +8,19 @@ import RightsHome from './RightsHome';
 import PhoneHome from './PhoneHome';
 
 export default function Home() {
+
+  useEffect(() => {
+    if (document.getElementById('mcjs')) return;
+
+    const script = document.createElement('script');
+    script.id = 'mcjs';
+    script.async = true;
+    script.src =
+      'https://chimpstatic.com/mcjs-connected/js/users/7eebd2a3d3d769aad41558f83/c4a0cdb4ce2f47c60723d55e9.js';
+
+    document.body.appendChild(script);
+  }, []);
+
   return (
     <>
       <IntroHome />
@@ -17,7 +30,6 @@ export default function Home() {
       <RightsHome />
       <NewsHome />
       <ContactHome />
-
     </>
-  )
+  );
 }
